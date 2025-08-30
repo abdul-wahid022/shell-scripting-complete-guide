7ede9a8a86311dc5caa018aa5a843e09fca6908d
Abdul Wahid <a.wahid7860668@gmail.com>
2025-08-30 19:41:48 +0500

    Update hello_world.sh

--- Scripts/01_Basics/hello_world.sh
+++ Scripts/01_Basics/hello_world.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+# hello_world.sh
+# Description: The classic first program. Prints a greeting.
+# Usage: ./hello_world.sh
+
+echo "Hello, World!"
+echo "Welcome to the world of shell scripting, $USER!"
+echo "This script is running from: $PWD"
